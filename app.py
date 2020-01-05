a9573b425a2f96cea133f9245c2fe5a0b559508c
Leiya Kenney <leiya.kenney@gmail.com>
2020-01-05 15:10:29 -0700

    Update medicines_index to be medicine_search, get API calls to be dynamic with whatever drug is searched
    Co-authored-by: Smitha Hosmani 
    <32909448+hsmitha26@users.noreply.github.com>
    
    Co-Authored-By: null <hsmitha26@users.noreply.github.com>

--- app.py
+++ app.py
@@ -13,18 +13,13 @@ app.config["DEBUG"] = True
 #     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
 @app.route('/api/v1/search', methods=['GET'])
-def medicines_index():
-    # search = 'openfda.brand_name:xanax'
-    # api_key = 'FDA_API_KEY'
-    # params = {
-    # 'api_key': '{FDA_API_KEY}',
-    # 'search': 'openfda.brand_name:xanax'
-    # }
-    response = requests.get('https://api.fda.gov/drug/label.json?search=openfda.brand_name:xanax&api_key=n5fe5G1BTGmOwMglgvku6uQpa67LURmUEiMpv2VV')
+def medicine_search():
+    drug = request.args.get('drug', '')
+    response = requests.get('https://api.fda.gov/drug/label.json?search=openfda.brand_name:' + drug)
     json_response = response.json()
     brand_name = json_response['results'][0]['openfda']['brand_name'][0]
     generic_name = json_response['results'][0]['openfda']['generic_name'][0]
+    import pdb; pdb.set_trace()
     return jsonify({"name": brand_name, "generic_name": generic_name})
-    # return json_response
 
 app.run()
